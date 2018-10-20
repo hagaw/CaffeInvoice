1ffb0a1bd034874482a00b713c1f505c2353991a
hagaw <htesfaye@yahoo.com>
2018-10-19 17:52:17 -0700

    Last commit after making small edditing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,4 @@
 package com.company;
-
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -7,7 +6,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner sc=new Scanner(System.in);
         ArrayList<Item>myItemList=new ArrayList();
 
@@ -24,11 +22,7 @@ String InputResponse="";
  //========================================================
 
 
-
-
-
        do {
-
 
            System.out.println("Enter the Item name");
            String userInputItemName = sc.next();
@@ -37,40 +31,23 @@ String InputResponse="";
            System.out.println("enter the price");
            double userInputPrice = sc.nextDouble();
 
-
-
-
            System.out.println("is the item taxable T/F");
-
-
-
            userInputTaxab = sc.nextBoolean();
 
-
-
-           if (userInputTaxab = true) {
-
-
-
-
+           if (userInputTaxab == true) {
 
                ArrayList<Double> TaxRate = new ArrayList<Double>();
-
                TaxRate.add(6.00 / 100);
                TaxRate.add(5.30 / 100);
                TaxRate.add(5.75 / 100);
                TaxRate.add(5.00 / 100);
 
-
                Random rn = new Random();
                Rand = rn.nextInt(TaxRate.size());
                salesTaxRate = TaxRate.get(Rand);
-
                System.out.println(salesTaxRate);
 
                //=========================================================
-
-
 
                    itemCost = userInputQuantity * userInputPrice;
                    totalCost = totalCost + itemCost;
@@ -78,9 +55,7 @@ String InputResponse="";
                    grandTotal = totalCost + salesTax;
 
 
-
-
-               } else if (userInputTaxab = false) {
+               } else if (userInputTaxab == false) {
 
                    itemCost = userInputQuantity * userInputPrice;
                    totalCost = totalCost + itemCost;
@@ -89,28 +64,21 @@ String InputResponse="";
 
            myItemList.add(new Item(userInputItemName, userInputQuantity, userInputPrice, itemCost, userInputTaxab));
 
-
 //===================================================================
 
            System.out.println("Do you want to add more items y/n");
            InputResponse = sc.next();
 
-
 //=====================================================================
-
 
        } while (!InputResponse.equalsIgnoreCase("no")) ;
 
 
 
-
-
-       System.out.println("Item Nme"+  "\t" + "Quantity" + "\t" + "price" + "\t" + "Sub Total" +  "\t" + "Taxable" );
+       System.out.println("Item Nme" +  "\t" + "Quantity" + "\t" + "price" + "\t" + "Sub Total" +  "\t" + "Taxable" );
            System.out.println("========================================================================");
 
-
         //for (Item tpItem:myItemList) {
-
 
 
             //System.out.println(tpItem.getItemName() + "\t" + "\t" + tpItem.getQuantity() + "\t" + tpItem.getPrice() + "\t" + "\t" + "\t" + "\t" + itemCost + "\t" + "\t" + tpItem.isTaxable());
@@ -118,20 +86,18 @@ String InputResponse="";
 
 
 
-
         for (Item tpItem:myItemList) {
 
 
-
-            System.out.println(tpItem.getItemName() + "\t" + "\t" + tpItem.getQuantity() + "\t" + tpItem.getPrice() + "\t" + "\t" + "\t" + "\t" + itemCost + "\t" + "\t" + tpItem.isTaxable());
+            System.out.println(tpItem.getItemName() + "\t" + "\t" + tpItem.getQuantity() + "\t" + "\t" + "\t" + tpItem.getPrice() + "\t" + "\t" + "\t" + tpItem.getCost()  + "\t" + "\t" + tpItem.isTaxable());
             // System.out.println("\t");
 
         }
 
            System.out.println("\t");
            System.out.println("Sub Total : "+ totalCost);
-           System.out.println("Tax Rate " + salesTaxRate);
-           System.out.println("Sales Tax " + salesTax);
+        System.out.printf("Sales Tax %.2f ", salesTax);
+        System.out.println("\nGrand total" + grandTotal);
 
 
         //-----------------------------------------------------------------------------
